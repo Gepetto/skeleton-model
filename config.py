c8da427f9fa605a21fa2bd664a6c3fa506b3e09a
GaloM <galo_xav@hotmail.com>
2016-12-04 15:08:18 +0100

    import numpy as np

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import pinocchio as se3
+import numpy as np
 oMp = se3.utils.rotate('z',np.pi/2) * se3.utils.rotate('x',np.pi/2)
 mesh_path = 'whole_body/stl_osim'
 model_path = 'whole_body/wholebody.osim'
