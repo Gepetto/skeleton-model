02b843b89c2718a26b946400a90bc9a62e42daf0
Galo Maldonado <gmaldona@laas.fr>
2016-12-05 15:27:42 +0100

    correct path to stl meshes

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import pinocchio as se3
 import numpy as np
+import os
 oMp = se3.utils.rotate('z',np.pi/2) * se3.utils.rotate('x',np.pi/2)
-mesh_path = 'whole_body/stl_osim'
-model_path = 'whole_body/wholebody.osim'
+actual_path = os.path.dirname(os.path.abspath('HQP'))
+mesh_path = actual_path+'/Models/whole_body/stl_osim'
+model_path = actual_path+'/Models/whole_body/wholebody.osim'
