0bc8b2238b5d09ae5b0c58382c58fc4f8ad4af2d
Galo <galo_xav@hotmail.com>
2017-01-03 22:33:22 +0100

    remove paths from module, osim file and mesh paths should be specified in the running script

--- models/config.py
+++ models/config.py
@@ -2,6 +2,3 @@ import pinocchio as se3
 import numpy as np
 import os
 oMp = se3.utils.rotate('z',np.pi/2) * se3.utils.rotate('x',np.pi/2)
-actual_path = os.path.dirname(os.path.abspath('HQP'))
-mesh_path = actual_path+'/Models/whole_body/stl_osim'
-model_path = actual_path+'/Models/whole_body/wholebody.osim'
