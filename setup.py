e1dba7ad26ec09aa605da959b34cc429b9c5892f
Galo <galo_xav@hotmail.com>
2017-01-03 20:09:10 +0100

    tree reorganization of the module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+#from distutils.core import setup
+
+setup(
+    name='Models',
+    description='Biomechanical and Robotic Models',
+    version='1.0',
+
+    packages=find_packages(exclude=['data','tests']),
+    
+    author='Galo MALDONADO',
+    author_email='galo.maldonado@laas.fr',
+)
